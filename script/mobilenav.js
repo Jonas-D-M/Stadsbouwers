b21b8978c40764fd9744592119a878ada3627f8c
JonasDM <jonasdemeyer@outlook.com>
2020-03-31 11:32:16 +0200

    made index.html responsive

--- script/mobilenav.js
+++ script/mobilenav.js
@@ -1,12 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-  toggleNav();
-});
-
 const toggleNav = function() {
   let toggleTrigger = document.querySelectorAll('.js-toggle-nav');
+  let logo = document.querySelector('.c-header__logo');
+
   for (let i = 0; i < toggleTrigger.length; i++) {
     toggleTrigger[i].addEventListener('click', function() {
+      if (logo.style.display === 'none') {
+        logo.style.display = 'block';
+      } else {
+        logo.style.display = 'none';
+      }
       document.querySelector('body').classList.toggle('has-mobile-nav');
     });
   }
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+  toggleNav();
+});
