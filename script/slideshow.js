cd74e098c432038a572b64e2b72133efdfbd84fe
JonasDM <jonasdemeyer@outlook.com>
2020-06-07 15:12:21 +0200

    added slideshow for testimonials

---
+++ script/slideshow.js
@@ -0,0 +1,49 @@
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+const hideElement = (element) => {
+  if (element instanceof HTMLElement) {
+    element.style.transition = 'all 2s';
+    element.style.display = 'none';
+  }
+};
+
+const displayElement = (element) => {
+  if (element instanceof HTMLElement) {
+    element.style.transition = 'all 2s';
+    element.style.display = 'block';
+  }
+};
+
+const hideAll = (testemonialObject) => {
+  for (let i = 0; i < testemonialObject.length; i++) {
+    const element = testemonialObject[i];
+    element.style.display = 'none';
+  }
+};
+
+const slideShow = async (testemonialObject) => {
+  while (true) {
+    for (let i = 0; i < testemonialObject.length; i++) {
+      let elementToBeDisplayed = testemonialObject[i];
+      let elementToBeHidden;
+      if (i - 1 >= 0) {
+        elementToBeHidden = testemonialObject[i - 1];
+      } else {
+        elementToBeHidden = testemonialObject[testemonialObject.length - 1];
+      }
+
+      hideElement(elementToBeHidden);
+      displayElement(elementToBeDisplayed);
+      await sleep(5000);
+      console.log(i);
+    }
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.info('Dom contentloaded');
+  const testemonials = document.querySelectorAll('.c-testimonial');
+  hideAll(testemonials);
+  slideShow(testemonials);
+});
